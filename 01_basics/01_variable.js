91c0c1f1ab08df4dc0125a16cf90dd88c762b37d
neerajbassan <133155711+neerajbassan@users.noreply.github.com>
2024-03-18 14:50:41 +0000

    variables done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,6 @@
+const  accountId=1002;
+let accountPassword="#Neeraj@123"
+var accountEmail="neeraj@gmail.com"
+var accountCity="Jammu"
+
+console.table([accountId,accountPassword,accountEmail,accountCity]);
